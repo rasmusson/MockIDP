db7a5eccb805c75dd7b9906fd707edb662b4e5e9
Jobb <Jobb@Stefans-MacBook-Pro.local>
2013-09-02 21:47:35 +0200

    added relaystate

--- MockIDP/src/main/java/no/evote/security/MockIDPAuthnReq.java
+++ MockIDP/src/main/java/no/evote/security/MockIDPAuthnReq.java
@@ -24,6 +24,7 @@ public class MockIDPAuthnReq extends HttpServlet {
 	public static String userId;
 	public static String secLevel;
 	public static String authnReqId;
+	public static String relayState;
 
 	static {
 		// Init OpenSAML
@@ -41,6 +42,8 @@ public class MockIDPAuthnReq extends HttpServlet {
 		resp.setContentType("text/html");
 		AuthnRequest authnRequest = unMarshallAuthnRequest(req);
 		authnReqId = authnRequest.getID();
+		
+		relayState = req.getParameter("RelayState");
 
 		w.append("<html>" + "<head></head>" + "<body> <form method=\"POST\">" + "Bruker ID: <input type=\"text\" name=\"username\" /> " + "<br/>"
 				+ "Security level: <input type=\"text\" name=\"secLevel\" value=\"3\"/> " + "<br/>" + "<input type=\"submit\" value=\"Login\"/>" + "</form>"
@@ -70,6 +73,6 @@ public class MockIDPAuthnReq extends HttpServlet {
 
 		userId = req.getParameter("username");
 		secLevel = req.getParameter("secLevel");
-		resp.sendRedirect("http://openam.steras.no:8080/openam-server-10.1.0-Xpress/Consumer/metaAlias/sp?SAMLart=AAQAAMFbLinlXaCM%2BFIxiDwGOLAy2T71gbpO7ZhNzAgEANlB90ECfpNEVLg%3D");
+		resp.sendRedirect("http://openam.steras.no:8080/openam-server-10.1.0-Xpress/Consumer/metaAlias/sp?SAMLart=AAQAAMFbLinlXaCM%2BFIxiDwGOLAy2T71gbpO7ZhNzAgEANlB90ECfpNEVLg%3D&RelayState=" + relayState);
 	}
 }
